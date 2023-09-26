bf658de0fe4433cdf452efcba409e6feda388a22
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-09-26 18:56:21 +0100

    Video animation (wip)

--- src/app/components/video.tsx
+++ src/app/components/video.tsx
@@ -1,7 +1,7 @@
 // components/AutoPlayVideo.js
 function AutoPlayVideo() {
   return (
-    <video width="100%" height="auto" autoPlay muted loop>
+    <video width="50%" height="auto" autoPlay muted loop>
       <source src="/demos/scaffold.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import Navbar from './components/navBar';
 import { motion, useAnimation } from 'framer-motion';
+import AutoPlayVideo from './components/video';
 
 export default function Home() {
     const [targetY, setTargetY] = useState(0);
@@ -47,20 +48,42 @@ export default function Home() {
         };
     }, [targetY, controls]);
 
+    const calculateOpacity = () => {
+        return Math.min(1, Math.abs(targetY / 250));  // adjust denominator as needed
+    };
+
+    const calculateScale = () => {
+        return 0.5 + (0.5 * calculateOpacity());  // scale between 0.5 to 1 based on opacity
+    };
+
     return (
         <>
             <Navbar />
             <main className="bg-dark-blue flex flex-col justify-center min-h-screen">
-                <motion.div
-                    className="flex flex-col items-center justify-center h-screen bg-dark-blue px-80"
-                    animate={controls}
-                >
-                    <h1 style={{ fontSize: '36px', fontFamily: 'Exo, sans-serif', whiteSpace: 'pre-wrap' }} className="text-4xl font-bold">
-                        <span style={{ color: '#DFEAFF' }}>Turn your IDE into an {'\n'}</span>
-                        <span style={{ color: '#5B89FF' }}>AI </span>
-                        <span style={{ color: '#DAEBE7' }}>Software Engineer</span>
-                    </h1>
-                </motion.div>
+           <motion.div
+    className="flex flex-col items-center justify-center h-screen bg-dark-blue px-80 relative" // Added relative positioning here
+    animate={controls}
+>
+    <h1 
+        style={{ fontSize: '36px', fontFamily: 'Exo, sans-serif', whiteSpace: 'pre-wrap' }} 
+        className="text-4xl font-bold z-10" // Added z-index to ensure slogan stays on top
+    >
+        <span style={{ color: '#DFEAFF' }}>Turn your IDE into an {'\n'}</span>
+        <span style={{ color: '#5B89FF' }}>AI </span>
+        <span style={{ color: '#DAEBE7' }}>Software Engineer</span>
+    </h1>
+    <motion.div 
+        className="absolute bottom-0 left-0 w-full h-full flex items-center justify-center" // Set to absolute positioning
+        style={{ 
+            opacity: calculateOpacity(), 
+            scale: calculateScale()
+        }}
+    >
+        <AutoPlayVideo />
+    </motion.div>
+</motion.div>
+
+
             </main>
         </>
     );

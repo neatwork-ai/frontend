61abd19037842e7835a978043ea585582241f116
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-09-26 23:42:54 +0100

    Video Component

--- src/app/components/video.tsx
+++ src/app/components/video.tsx
@@ -2,7 +2,7 @@
 function AutoPlayVideo() {
   return (
     <video width="50%" height="auto" autoPlay muted loop>
-      <source src="/demos/scaffold.mp4" type="video/mp4" />
+      <source src="/demos/neatcoder_showcase.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ import AutoPlayVideo from './components/video';
 
 export default function Home() {
     const [targetY, setTargetY] = useState(0);
-    const controls = useAnimation();
+    const sloganControls = useAnimation();
+    const videoControls = useAnimation();
     const lastY = useRef(0);
     const rafRef = useRef<number | null>(null);
 
@@ -23,67 +24,48 @@ export default function Home() {
             lastY.current = newTarget;
         };
 
-        const updatePosition = () => {
-            const difference = targetY - lastY.current;
-            
-            // Check if the difference is within a small threshold (e.g., 1) 
-            if (Math.abs(difference) < 1) { 
-                controls.set({ y: targetY });
-                cancelAnimationFrame(rafRef.current!);
-            } else {
-                const newY = difference * 0.5 + lastY.current;
-                controls.start({ y: newY });
-                lastY.current = newY;
-                rafRef.current = requestAnimationFrame(updatePosition);
-            }
-        };
-        
-
         window.addEventListener("wheel", handleWheel);
-        rafRef.current = requestAnimationFrame(updatePosition);
+
+        // Animation for the slogan
+        sloganControls.start({ y: targetY });
+
+        // Animation for the video
+        const opacity = Math.min(1, Math.abs(targetY / 250));
+        const scale = 0.5 + (0.5 * opacity);
+        videoControls.start({ opacity, scale });
 
         return () => {
             window.removeEventListener("wheel", handleWheel);
-            cancelAnimationFrame(rafRef.current!);
         };
-    }, [targetY, controls]);
-
-    const calculateOpacity = () => {
-        return Math.min(1, Math.abs(targetY / 250));  // adjust denominator as needed
-    };
-
-    const calculateScale = () => {
-        return 0.5 + (0.5 * calculateOpacity());  // scale between 0.5 to 1 based on opacity
-    };
+    }, [targetY, sloganControls, videoControls]);
 
     return (
         <>
             <Navbar />
-            <main className="bg-dark-blue flex flex-col justify-center min-h-screen">
-           <motion.div
-    className="flex flex-col items-center justify-center h-screen bg-dark-blue px-80 relative" // Added relative positioning here
-    animate={controls}
->
-    <h1 
-        style={{ fontSize: '36px', fontFamily: 'Exo, sans-serif', whiteSpace: 'pre-wrap' }} 
-        className="text-4xl font-bold z-10" // Added z-index to ensure slogan stays on top
-    >
-        <span style={{ color: '#DFEAFF' }}>Turn your IDE into an {'\n'}</span>
-        <span style={{ color: '#5B89FF' }}>AI </span>
-        <span style={{ color: '#DAEBE7' }}>Software Engineer</span>
-    </h1>
-    <motion.div 
-        className="absolute bottom-0 left-0 w-full h-full flex items-center justify-center" // Set to absolute positioning
-        style={{ 
-            opacity: calculateOpacity(), 
-            scale: calculateScale()
-        }}
-    >
-        <AutoPlayVideo />
-    </motion.div>
-</motion.div>
+            <main className="bg-dark-blue flex flex-col justify-center min-h-screen relative">
+                {/* Slogan */}
+                <motion.div
+                    className="flex flex-col items-center justify-center h-auto bg-dark-blue px-80"
+                    animate={sloganControls}
+                >
+                    <h1 
+                        style={{ fontSize: '36px', fontFamily: 'Exo, sans-serif', whiteSpace: 'pre-wrap' }} 
+                        className="text-4xl font-bold z-10"
+                    >
+                        <span style={{ color: '#DFEAFF' }}>Turn your IDE into an {'\n'}</span>
+                        <span style={{ color: '#5B89FF' }}>AI </span>
+                        <span style={{ color: '#DAEBE7' }}>Software Engineer</span>
+                    </h1>
+                </motion.div>
 
-
+                {/* Video */}
+                <motion.div 
+                    className="absolute top-1/2 left-0 w-full flex items-center justify-center mt-[-10%]"
+                    initial={{ opacity: 0, scale: 0.5 }} // setting initial values
+                    animate={videoControls}
+                >
+                    <AutoPlayVideo />
+                </motion.div>
             </main>
         </>
     );

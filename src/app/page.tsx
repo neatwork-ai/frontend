da7e17c7b4d8dfd22b58544adb5df6578e36fde5
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-09-26 11:24:29 +0100

    Slogan animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,60 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import Image from "next/image";
-import GlassButton from './components/download';
+import React, { useEffect, useState, useRef } from 'react';
 import Navbar from './components/navBar';
-import LandingSection from './components/landing';
 import { motion, useAnimation } from 'framer-motion';
-import AutoPlayVideo from './components/video';
 
 export default function Home() {
+    const [targetY, setTargetY] = useState(0);
+    const controls = useAnimation();
+    const lastY = useRef(0);
+
+    useEffect(() => {
+        const handleWheel = (e: WheelEvent) => {
+            // Define the maximum allowable offset
+            const MAX_Y = -250; // Adjust this value to your preference
+            let newTarget = lastY.current - e.deltaY; // Multiplier to control sensitivity
+
+            // Ensure we don't scroll past the max value
+            newTarget = Math.max(MAX_Y, newTarget);
+            newTarget = Math.min(0, newTarget); // Ensuring we don't scroll in the reverse direction
+
+            setTargetY(newTarget);
+            lastY.current = newTarget;
+        };
+
+        const updatePosition = () => {
+            // Lerp formula: newValue = (target - current) * factor + current
+            const newY = (targetY - lastY.current) * 0.1 + lastY.current;
+            controls.start({ y: newY });
+            lastY.current = newY;
+
+            requestAnimationFrame(updatePosition);
+        };
+
+        window.addEventListener("wheel", handleWheel);
+        requestAnimationFrame(updatePosition);
+
+        return () => {
+            window.removeEventListener("wheel", handleWheel);
+        };
+    }, [targetY, controls]);
 
     return (
         <>
-        <Navbar />
-        <main className="bg-dark-blue flex flex-col justify-center min-h-screen">
-            <div className="flex flex-col md:flex-row items-center justify-center h-screen bg-dark-blue px-80"> {/* <-- Changed justify-between to justify-center */}
-                <div className="flex flex-col w-full md:w-auto text-center md:text-left mb-10 md:mb-0 justify-center items-center" id="slogan"> {/* <-- Added items-center and removed fixed md:static */}
-                    <div className="flex flex-col w-full md:w-auto text-center md:text-left mb-10 md:mb-0 justify-center items-center"> {/* <-- Added items-center */}
-                        <h1 style={{ fontSize: '36px', fontFamily: 'Exo, sans-serif', whiteSpace: 'pre-wrap' }} className="text-4xl font-bold">
-                            <span style={{ color: '#DFEAFF' }}>Turn your IDE into an {'\n'}</span>
-                            <span style={{ color: '#5B89FF' }}>AI </span>
-                            <span style={{ color: '#DAEBE7' }}>Software Engineer</span>
-                        </h1>
-                    </div>
-                    <AutoPlayVideo />
-                </div>
-            
-            </div>
-        </main>
+            <Navbar />
+            <main className="bg-dark-blue flex flex-col justify-center min-h-screen">
+                <motion.div
+                    className="flex flex-col items-center justify-center h-screen bg-dark-blue px-80"
+                    animate={controls}
+                >
+                    <h1 style={{ fontSize: '36px', fontFamily: 'Exo, sans-serif', whiteSpace: 'pre-wrap' }} className="text-4xl font-bold">
+                        <span style={{ color: '#DFEAFF' }}>Turn your IDE into an {'\n'}</span>
+                        <span style={{ color: '#5B89FF' }}>AI </span>
+                        <span style={{ color: '#DAEBE7' }}>Software Engineer</span>
+                    </h1>
+                </motion.div>
+            </main>
         </>
     );
 }
--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -7,7 +7,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
         <body>
           <Main />

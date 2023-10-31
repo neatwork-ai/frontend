147c2542180f1962e6d92555fb84414210750a83
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-10-31 08:56:47 +0000

    Setup Google Analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -235,9 +235,9 @@ export default function Home() {
                 </motion.div>
                 )}
 
-                {scrollPhase === 3 && (
+                {/* {scrollPhase === 3 && (
                     <DownloadNowButton />
-                )}
+                )} */}
 
                 {/* Video */}
                 {(scrollPhase === 1) && (

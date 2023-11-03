16de2a121ecb483bc06ac93f88df678a5352c679
Tamas Szucs <tomfurrier@gmail.com>
2023-11-03 17:18:47 +0100

    fix scrolling for windows too

--- src/app/hooks/wheel.tsx
+++ src/app/hooks/wheel.tsx
@@ -11,129 +11,47 @@ const useWheelScroll = (
     sloganRef: React.MutableRefObject<HTMLParagraphElement | null>,
 ) => {
   const handleWheel = (e: WheelEvent) => {
-    // The first step is to understand in which phase we are in
-    // as well as the direction of the wheel
-
-    // To determine the direction of the wheel scroll, we examine the
-    // deltaY property of the WheelEvent object in the event handler:
-    let isForward = e.deltaY > 0 // is true if wheel was scrolled downwards and false if upwards
+    let isForward = e.deltaY > 0;
 
     switch (scrollPhase) {
-        case 0: {
-            // == Action Handling ==
-            let MAX_SHIFT = setMaxShift(navBarRef, sloganRef, false)!; // False signals that we do not recalculate this
-
-            let newTarget = lastY.current - e.deltaY * 1.2; // 1.2 is the sensitivity
-        
-            newTarget = Math.max(MAX_SHIFT, newTarget); // To prevent overflowing
-            newTarget = Math.min(0, newTarget); // To prevent underflowing
-
-            setTargetY(newTarget);
-            globals.currentSloganPosition = newTarget;
-            lastY.current = newTarget;
-
-            // == State Transition ==
-                if (newTarget === MAX_SHIFT) {
-                    setScrollPhase(1);
-
-                    // Reset transition counter
-                    globals.transitionCounter = 0;
-                }
-
-                break;
-            }
-                
-            case 1: {
-                if (isForward) {
-                    // In order to move to the next phase we need to hit
-                    // the following criteria:
-                    // - Have 6 increasing deltas in a row, representing a new wheel action
-                    
-                    // Transition counter registers everytime the delta increases
-                    if (e.deltaY > globals.deltaTMinusOne) {
-                        globals.transitionCounter += 1;
-                    }
-
-                    // Whenever we hit 3 increasing deltas it means we are ready to move to the next
-                    // scrolling phase
-                    if (globals.transitionCounter >= DELTA_THRESHOLD) {
-                        setScrollPhase(2);
-
-                        // Reset transition counter
-                        globals.transitionCounter = 0;
-                    }
-                } else {
-                    if (-e.deltaY > -globals.deltaTMinusOne) {
-                        globals.transitionCounter += 1;
-                    }
-
-                    // Whenever we hit 3 increasing deltas it means we are ready to move to the next
-                    // scrolling phase
-                    if (globals.transitionCounter >= DELTA_THRESHOLD) {
-                        setScrollPhase(0);
-
-                        // Reset transition counter
-                        globals.transitionCounter = 0;
-                    }
-                }
-                    
-                break;
-            }
-            case 2: {
-                if (isForward) {
-                    // In order to move to the next phase we need to hit
-                    // two criteria:
-                    // - Have 6 increasing deltas in a row, representing a new wheel action
-                    
-                    // Transition counter registers everytime the delta increases
-                    if (e.deltaY > globals.deltaTMinusOne) {
-                        globals.transitionCounter += 1;
-                    }
-
-                    // Whenever we hit 3 increasing deltas it means we are ready to move to the next
-                    // scrolling phase
-                    if (globals.transitionCounter >= DELTA_THRESHOLD) {
-                        setScrollPhase(3);
-                        // Reset transition counter
-                        globals.transitionCounter = 0;
-                    }
-                } else {
-                    if (-e.deltaY > -globals.deltaTMinusOne) {
-                        globals.transitionCounter += 1;
-                    }
-
-                    // Whenever we hit 3 increasing deltas it means we are ready to move to the next
-                    // scrolling phase
-                    if (globals.transitionCounter >= DELTA_THRESHOLD) {
-                        setScrollPhase(1);
-
-                        // Reset transition counter
-                        globals.transitionCounter = 0;
-                    }
-                }
-
-                break;
-            }
-            case 3: {
-                if (!isForward) {
-                    if (-e.deltaY > -globals.deltaTMinusOne) {
-                        globals.transitionCounter += 1;
-                    }
-
-                    // Whenever we hit 3 increasing deltas it means we are ready to move to the next
-                    // scrolling phase
-                    if (globals.transitionCounter >= DELTA_THRESHOLD) {
-                        setScrollPhase(2);
-
-                        // Reset transition counter
-                        globals.transitionCounter = 0;
-                    }
-                }
-                break;
-            }
+      case 0:
+        let MAX_SHIFT = setMaxShift(navBarRef, sloganRef, false)!;
+        let newTarget = lastY.current - e.deltaY * 1.2;
+        newTarget = Math.max(MAX_SHIFT, newTarget);
+        newTarget = Math.min(0, newTarget);
+        setTargetY(newTarget);
+        globals.currentSloganPosition = newTarget;
+        lastY.current = newTarget;
+        if (newTarget === MAX_SHIFT) {
+          setScrollPhase(1);
         }
+        break;
 
-        globals.deltaTMinusOne = e.deltaY;
+      case 1:
+        if (isForward) {
+          setScrollPhase(2);
+        } else {
+          setScrollPhase(0);
+        }
+        break;
+
+      case 2:
+        if (isForward) {
+          setScrollPhase(3);
+        } else {
+          setScrollPhase(1);
+        }
+        break;
+
+      case 3:
+        if (!isForward) {
+          setScrollPhase(2);
+        }
+        break;
+
+      default:
+        break;
+    }
   };
 
   return handleWheel;

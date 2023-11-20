51deadb2958d38d34b84aa989143c50973f2c811
Tamas Szucs <tomfurrier@gmail.com>
2023-11-20 16:14:19 +0100

    form design

--- src/app/components/newsletterForm.tsx
+++ src/app/components/newsletterForm.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { getPlaneKeyframes } from "@/lib/getPlaneKeyframes";
-import { getTrailsKeyframes } from "@/lib/getTrailsKeyframes";
 import { EnvelopeIcon } from "@heroicons/react/24/outline";
 import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import { gsap } from "gsap";
 import { FormEvent, useRef, useState } from "react";
 
 function NewsletterForm() {
@@ -12,9 +9,7 @@ function NewsletterForm() {
   const [successMessage, setSuccessMessage] =
     useState<MembersSuccessResponse>();
   const [errorMessage, setErrorMessage] = useState("");
-  const [active, setActive] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
-  const { to, fromTo, set } = gsap;
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -23,16 +18,6 @@ function NewsletterForm() {
     const button = buttonRef.current;
 
     if (!email || !button) return;
-
-    if (!active) {
-      setActive(true);
-
-      to(button, {
-        keyframes: getPlaneKeyframes(set, fromTo, button, setActive, setInput),
-      });
-
-      to(button, { keyframes: getTrailsKeyframes(button) });
-    }
 
     const res = await fetch("/api/addSubscription", {
       body: JSON.stringify({ email }),
@@ -56,22 +41,22 @@ function NewsletterForm() {
     setErrorMessage("");
   };
 
-return (
-    <div className="flex flex-col space-y-4 md:w-[300px]">
+  return (
+    <div className="flex flex-col space-y-4 md:w-[450px]">
       <form onSubmit={handleSubmit} className="mt-6">
-        <div className="flex items-center gap-x-3 py-1 px-3 rounded bg-[#090D11]">
-          <EnvelopeIcon className="w-5 h-5 text-[#4B4C52]" />
+        <div className="flex items-center gap-x-3 py-3 px-4 rounded-lg bg-[#F3F3FF]">
+          <EnvelopeIcon className="w-6 h-6 text-black" />
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
             placeholder="Email address"
             required
             type="email"
-            className="flex-1 text-white text-sm bg-transparent outline-none placeholder-[#4B4C52]"
+            className="flex-1 text-black text-base bg-transparent outline-none placeholder-white::placeholder rounded h-12 px-3"
           />
           <button
             ref={buttonRef}
-            className="text-sm"
+            className="text-sm  bg-blue-700 hover:bg-blue-600 text-white py-2 px-4 rounded"
             disabled={!input}
             type="submit"
           >
@@ -81,11 +66,11 @@ return (
       </form>
 
       {(successMessage || errorMessage) && (
-        <div className="flex items-center space-x-2 bg-[#0A0E12] text-white rounded py-2 px-4">
+        <div className={`flex items-center space-x-2 rounded py-2 px-4 text-white ${successMessage ? 'bg-green-500' : 'bg-red-500'}`}>
           <CheckIcon className="h-4 w-4" />
           <div className="text-xs">
             {successMessage ? (
-            <p>We&apos;ve added {successMessage.email_address} to our waitlist.</p>
+              <p>We&apos;ve added {successMessage.email_address} to our waitlist.</p>
             ) : (
               <p>You are already on our waitlist.</p>
             )}
@@ -96,6 +81,5 @@ return (
     </div>
   );
 }
-
 
 export default NewsletterForm;
